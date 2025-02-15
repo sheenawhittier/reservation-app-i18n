bda8230e005226fa07b86e42a827eeffe4b1c849
Sheena Whittier <swh1309@wgu.edu>
2025-02-15 11:03:42 -0700

    updated resource bundles to show messages in both french and english. changed styling to look better too

--- src/main/UI/src/app/config/welcome.component.ts
+++ src/main/UI/src/app/config/welcome.component.ts
@@ -9,21 +9,18 @@ import { HttpClient } from '@angular/common/http';
 export class WelcomeComponent implements OnInit {
   englishMessage: string = '';
   frenchMessage: string = '';
+  presentationMessage: string = '';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<{ English: string; French: string }>('http://localhost:8080/api/welcome')
-      .subscribe(
-        (data) => {
-          this.englishMessage = data.English;
-          this.frenchMessage = data.French;
-        },
-        (error) => {
-          console.error('Error fetching welcome messages:', error);
-        }
-      );
+    this.http.get<{ English: string; French: string; Presentation: string }>('http://localhost:8080/api/welcome')
+      .subscribe((data) => {
+        this.englishMessage = data.English;
+        this.frenchMessage = data.French;
+        this.presentationMessage = data.Presentation;
+      });
   }
 }
+
 
--- src/main/java/edu/wgu/d387_sample_code/rest/WelcomeController.java
+++ src/main/java/edu/wgu/d387_sample_code/rest/WelcomeController.java
@@ -2,25 +2,36 @@ package edu.wgu.d387_sample_code.rest;
 
 import edu.wgu.d387_sample_code.service.WelcomeMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.*;
 
 @RestController
 @RequestMapping("/api")
 public class WelcomeController {
 
-    @Autowired
-    private WelcomeMessageService welcomeMessageService;
-
     @GetMapping("/welcome")
-    public Map<String, String> getWelcomeMessages() {
+    public ResponseEntity<Map<String, String>> getWelcomeMessages() throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        Callable<String> fetchEnglishMessage = () -> "Welcome to Landon Hotel!";
+        Callable<String> fetchFrenchMessage = () -> "Bienvenue à l'hôtel Landon!";
+
+        Future<String> englishFuture = executor.submit(fetchEnglishMessage);
+        Future<String> frenchFuture = executor.submit(fetchFrenchMessage);
+
         Map<String, String> messages = new HashMap<>();
-        messages.put("English", welcomeMessageService.displayMessage("en"));
-        messages.put("French", welcomeMessageService.displayMessage("fr"));
-        return messages;
+        messages.put("English", englishFuture.get());
+        messages.put("French", frenchFuture.get());
+        messages.put("Presentation", "Landon Hotel is committed to providing the best experience!");
+
+        executor.shutdown();
+        return ResponseEntity.ok(messages);
     }
 }
+
